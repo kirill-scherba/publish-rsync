e1040d9b8e86b7c6c3e3d7f4014b0185035acf4b
Kirill Scherba <kirill@scherba.ru>
2021-10-16 19:10:19 +0300

    Deployment key parameter added to this action

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,7 @@
 #   INPUT_SERVER_FOLDER: $2
 #   INPUT_SERVER_USER_NAME: $3
 #   INPUT_SOURCE_FOLDER: $4
+#   INPUT_DEPLOYMENT_KEY: $5
 #
 #
 
@@ -14,20 +15,30 @@ echo -e "\nGet input parameters:" \
     "\nserver_name(\$1): $1" \
     "\nserver_folder(\$2): $2" \
     "\nserver_user_name(\$3): $3" \
-    "\nsource_foldere(\$4): $4"
+    "\nsource_foldere(\$4): $4" \
+    "\ndeployment_key(\$5): $5"
 
 # Check named parameters
 echo -e "\nGet named input parameters:\n" \
     "\nParameter server_name: '${INPUT_SERVER_NAME}'" \
     "\nParameter server_folder: '${INPUT_SERVER_FOLDER}'" \
     "\nParameter server_user_name: '${INPUT_SERVER_USER_NAME}'" \
-    "\nParameter source_folder: '${INPUT_SOURCE_FOLDER}'"
+    "\nParameter source_folder: '${INPUT_SOURCE_FOLDER}'" \
+    "\nParameter deployment_key: '${INPUT_DEPLOYMENT_KEY}'"
 
-# Show and publish command
+# Show publish command
 echo -e "\nPreparing to execute publish commands: \ncd ${INPUT_SOURCE_FOLDER} &&" \
         "rsync --progress -e \"ssh -o StrictHostKeyChecking=no\" --recursive ./ " \
         "${INPUT_SERVER_USER_NAME}@${INPUT_SERVER_NAME}:${INPUT_SERVER_FOLDER} &&" \
         "cd ../"
+
+# Set private SSH key
+# echo "${INPUT_DEPLOYMENT_KEY}" | base64 -d >> "/root/.ssh/id_rsa" && \
+echo "Set deployment key"
+echo "${INPUT_DEPLOYMENT_KEY}" >> "/root/.ssh/id_rsa" && \
+    chmod 0700 "/root/.ssh" && \
+    chmod 0600 "/root/.ssh/id_rsa"
+cat /root/.ssh/id.rsa
 
 # Change dir to source folder
 cd ${INPUT_SOURCE_FOLDER}
