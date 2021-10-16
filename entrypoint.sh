ad63592a02bb7c07f0b21bd11a18d21cf52ae44f
Kirill Scherba <kirill@scherba.ru>
2021-10-16 16:59:42 +0300

    Start execute basic commands in entrypount.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,22 @@
 #!/bin/sh -l
 
-# echo "Hello $1"
+# Input parameters
+#
+#   server-name: $1
+#   server-folder: $2
+#   erver-user-name: $3
+#   source-folder: $4
+#
+#
 
+# Show input parameters and publish command
 echo -e "Get input parameters:\nserver-name: $1\nserver-folder: $2\nserver-user-name: $3\nsource-folder: $4\n"
 echo -e "Preparing to execute publish commands: \ncd $4 && rsync --progress -e \"ssh -o StrictHostKeyChecking=no\" --recursive ./ $3@$1:$2 && cd ../"
+
+# Change dir to source folder
+cd $4
+ls -all
+cd ../ 
 
 time=$(date)
 echo "::set-output name=time::$time"
