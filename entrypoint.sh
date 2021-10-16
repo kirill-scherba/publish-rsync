7ebb264b09f7e63d278087aaa05326e4ca8f8a47
Kirill Scherba <kirill@scherba.ru>
2021-10-17 02:58:32 +0300

    Remove extra comments.

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,22 +12,6 @@
 
 set -e
 
-# Show input parameters (by $)
-# echo -e "\nGet input parameters:" \
-#     "\nserver_name(\$1): $1" \
-#     "\nserver_folder(\$2): $2" \
-#     "\nserver_user_name(\$3): $3" \
-#     "\nsource_foldere(\$4): $4" \
-#     "\ndeployment_key(\$5): $5"
-
-# Check named parameters
-# echo -e "\nGet named input parameters:\n" \
-#     "\nParameter server_name: '${INPUT_SERVER_NAME}'" \
-#     "\nParameter server_folder: '${INPUT_SERVER_FOLDER}'" \
-#     "\nParameter server_user_name: '${INPUT_SERVER_USER_NAME}'" \
-#     "\nParameter source_folder: '${INPUT_SOURCE_FOLDER}'" \
-#     "\nParameter deployment_key: '${INPUT_DEPLOYMENT_KEY}'"
-
 # Show publish command
 echo -e "\nPreparing to execute publish commands: \n" \
         "cd ${INPUT_SOURCE_FOLDER} &&" \
@@ -36,13 +20,11 @@ echo -e "\nPreparing to execute publish commands: \n" \
         "cd ../"
 
 # Set private SSH key
-# echo "${INPUT_DEPLOYMENT_KEY}" | base64 -d >> "/root/.ssh/id_rsa" && \
 echo -e "\nSet deployment key"
 mkdir -p /root/.ssh
 echo "${INPUT_DEPLOYMENT_KEY}" >> "/root/.ssh/id_rsa" && \
     chmod 0700 "/root/.ssh" && \
     chmod 0600 "/root/.ssh/id_rsa"
-# cat /root/.ssh/id_rsa
 
 # Change dir to source folder and execute rsync
 cd ${INPUT_SOURCE_FOLDER}
@@ -52,6 +34,3 @@ echo -e "\nRun rsync:"
 rsync --progress -e "ssh -o StrictHostKeyChecking=no" --recursive ./ \
     ${INPUT_SERVER_USER_NAME}@${INPUT_SERVER_NAME}:${INPUT_SERVER_FOLDER}
 cd ../ 
-
-# time=$(date)
-# echo "::set-output name=time::$time"
