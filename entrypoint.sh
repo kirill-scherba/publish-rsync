380df034f69fbb231c18a2d962cd44d96ad89d89
Kirill Scherba <kirill@scherba.ru>
2021-10-16 18:01:27 +0300

    Try use named parameters in entrypoint.sh (2)

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,11 +9,14 @@
 #
 #
 
-# Show input parameters and publish command
-echo -e "Get input parameters:\nserver-name: $1\nserver-folder: $2\nserver-user-name: $3\nsource-folder: $4\n"
-echo -e "Preparing to execute publish commands: \ncd $4 && rsync --progress -e \"ssh -o StrictHostKeyChecking=no\" --recursive ./ $3@$1:$2 && cd ../"
+# Show input parameters
+echo -e "\nGet input parameters:\nserver-name: $1\nserver-folder: $2\nserver-user-name: $3\nsource-folder: $4"
+
 # Check named parameters
-echo -e "Parameter source-folder: '${INPUT_SOURCE-FOLDER}' or '${INPUT_SOURCE_FOLDER}'"
+echo -e "\nParameter source-folder: '${INPUT_SOURCE-FOLDER}' or '${INPUT_SOURCE_FOLDER}'"
+
+# Show and publish command
+echo -e "\nPreparing to execute publish commands: \ncd $4 && rsync --progress -e \"ssh -o StrictHostKeyChecking=no\" --recursive ./ $3@$1:$2 && cd ../"
 
 # Change dir to source folder
 cd $4
