9a048cfd70f97f023709cae66e952d177c7c3753
Kirill Scherba <kirill@scherba.ru>
2021-10-16 16:21:08 +0300

    Entrypoint.sh updated, set real inputs

--- README.md
+++ README.md
@@ -19,4 +19,3 @@ The time we greeted you.
 uses: actions/hello-world-docker-action@v1
 with:
   who-to-greet: 'Mona the Octocat'
-
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/sh -l
 
-echo "Hello $1"
+# echo "Hello $1"
+
+echo -e "Get input parameters:\nserver-name: $1\nserver-folder: $2\nserver-user-name: $3\nsource-folder: $4\n"
+echo -e "Preparing to execute publish commands: \ncd $4 && rsync --progress -e \"ssh -o StrictHostKeyChecking=no\" --recursive ./ $3@$1:$2 && cd ../"
+
 time=$(date)
 echo "::set-output name=time::$time"
-
